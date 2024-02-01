dbf9fbc29f0a7f4aaa485a3d70cf2e2d9b67f8c5
KozlovMV <KozlovMV@kadastr.ru>
2024-02-02 02:22:32 +0300

    Первая часть day6

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -1,21 +1,67 @@
 import java.io.File
 import java.util.*
 import kotlin.io.path.Path
+import kotlin.math.min
 
 object Day6 {
     const val VALUE = 938
 
-    fun count(): Int {
+    class Race {
+        private val races = LinkedList<Pair<Int, Int>>()
+        private val numberOfWays = LinkedList<Int>()
+
+        fun setData(scanner: Scanner) {
+            val rawLineOfTimes = scanner.nextLine().split(Regex("Time:( )+"))
+            val stringTimes = getLine(rawLineOfTimes)
+            val rawLineOfDistances = scanner.nextLine().split(Regex("Distance:( )+"))
+            val stringDistances = getLine(rawLineOfDistances)
+            val numberOfRaces = min(stringTimes.size, stringDistances.size)
+            for (i in 0 until numberOfRaces) {
+                val pair = Pair(stringTimes[i].toInt(), stringDistances[i].toInt())
+                races.add(pair)
+            }
+            println(races)
+        }
+
+        private fun getLine(rawLine: List<String>): List<String> {
+            return rawLine[1].split(Regex("( )+"))
+        }
+
+        fun countNumOfWays() {
+            for (p in races) {
+                var count = 0
+                val time = p.first
+                val record = p.second
+                for (t in 0..time) {
+                    val distance = distance(time, t)
+                    if (distance > record) count++
+                }
+                numberOfWays.add(count)
+            }
+            println(numberOfWays)
+        }
+
+        private fun distance(time: Int, charge: Int): Int {
+            return (time - charge) * charge
+        }
+
+        fun multi(): Long {
+            return numberOfWays.fold(1L) { multi, value -> multi * value }
+        }
+    }
+
+    fun count(): Long {
         val path = MAIN_INPUT_PATH.format("Day6")
         return count(path)
     }
 
-    // TODO
-    fun count(path: String): Int {
+    fun count(path: String): Long {
         val input = Path(path)
         val file = File(input.toUri())
         val scanner = Scanner(file)
-        println("Unfinished")
-        return 0
+        val race = Race()
+        race.setData(scanner)
+        race.countNumOfWays()
+        return race.multi()
     }
 }
