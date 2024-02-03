b32681bd522c118797c6797ec8a436fcc218a7c6
KozlovMV <KozlovMV@kadastr.ru>
2024-02-03 20:26:41 +0300

    Первая часть day8

--- src/main/kotlin/Day8.kt
+++ src/main/kotlin/Day8.kt
@@ -1,21 +1,60 @@
 import java.io.File
 import java.util.*
+import kotlin.collections.HashMap
 import kotlin.io.path.Path
 
 object Day8 {
     const val VALUE = 940
+    private const val LEFT_CMD = 'L'
+    private const val START = "AAA"
+    private const val FINISH = "ZZZ"
 
-    fun count(): Int {
+    private class Navigator(val instructions: String) {
+        private val nodes = HashMap<String, Pair<String, String>>()
+
+        fun addNode(key: String, node: Pair<String, String>) {
+            nodes[key] = node
+        }
+
+        fun next(key: String, command: Char): String {
+            val node = nodes[key]
+            return if (command == LEFT_CMD) node?.first ?: FINISH
+            else node?.second ?: FINISH
+        }
+
+        fun countStepsToDest(): Long {
+            var key = START
+            var count = 0L
+            while (key != FINISH) {
+                for (i in instructions) {
+                    if (key == FINISH) break
+                    key = next(key, i)
+                    count++
+                }
+            }
+            return count
+        }
+    }
+
+    fun count(): Long {
         val path = MAIN_INPUT_PATH.format("Day8")
         return count(path)
     }
 
-    // TODO
-    fun count(path: String): Int {
+    fun count(path: String): Long {
         val input = Path(path)
         val file = File(input.toUri())
         val scanner = Scanner(file)
-        println("Unfinished")
-        return 0
+        val instructions = scanner.nextLine()
+        val navigator = Navigator(instructions)
+        scanner.nextLine()
+        while (scanner.hasNext()) {
+            val line = scanner.nextLine().split(Regex("(( )+=( )+\\()|\\)"))
+            val key = line[0]
+            val pair = line[1].split(Regex(",( )+"))
+            navigator.addNode(key, Pair(pair[0], pair[1]))
+            println("$key = $pair")
+        }
+        return navigator.countStepsToDest()
     }
 }
